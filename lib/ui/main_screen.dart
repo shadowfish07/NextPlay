0d8596bded35c31628698654a018a855ef1f43b5
shadowfish <tough.bread6309@zqydev.me>
2025-09-19 01:02:11 +0800

    refactor(main_screen): 移除调试用的智能状态建议面板代码

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 import 'core/main_layout.dart';
 import 'discover/widgets/discover_screen.dart';
 import 'library/widgets/library_screen.dart';
 import 'settings/widgets/settings_screen.dart';
-import 'game_status/widgets/batch_status_screen.dart';
-import 'game_status/view_models/batch_status_view_model.dart';
-import '../data/repository/game_repository.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -28,26 +23,6 @@ class _MainScreenState extends State<MainScreen> {
   @override
   void initState() {
     super.initState();
-    // 应用启动时自动唤起智能状态建议面板（用于调试）
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _showBatchStatusScreen();
-    });
-  }
-
-  void _showBatchStatusScreen() {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      useSafeArea: true,
-      builder: (context) => ChangeNotifierProvider(
-        create: (context) => BatchStatusViewModel(
-          gameRepository: context.read<GameRepository>(),
-        ),
-        child: const BatchStatusScreen(
-          isFromOnboarding: false,
-        ),
-      ),
-    );
   }
 
   void _onDestinationSelected(int index) {
